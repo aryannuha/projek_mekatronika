ac85e80effad5e9acff9eecd198bcd9ea5dc70cf
Ammar <59948271+aryannuha@users.noreply.github.com>
2024-11-20 21:55:02 +0700

    Update mekatronika_projek.ino

--- mekatronika_projek/mekatronika_projek.ino
+++ mekatronika_projek/mekatronika_projek.ino
@@ -1,13 +1,24 @@
 // panggil library
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
-// definisi variabel
+// definisi pin
 #define turbidity A1
 #define ph A0
 
+// servo
+Servo asam;
+Servo basa;
+
+// definisi variabel
+const float vref = 5.0;
+const int adc_res = 1024;
+const float ph_offset = 0.00; // calibration offset ph
+const float ph_scale = 3.5; // scaling factor ph
 int turbidityValue;
 int phValue;
+float mapPh, voltage;
 int mapT;
 
 // set the LCD address to 0x27 for a 16 chars and 2 line display
@@ -16,9 +27,18 @@ LiquidCrystal_I2C lcd(0x27,20,4);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.init();                      
 
+  // pinMode
+  pinMode(turbidity, INPUT);
+  pinMode(ph, INPUT);
+
+  // initiate lcd
+  lcd.init();                      
   lcd.backlight();
+
+  // attach servo
+  asam.attach(9);
+  basa.attach(10);
 }
 
 void loop() {
@@ -36,9 +56,12 @@ void loop() {
   lcd.setCursor(13, 0);
   lcd.print("NTU");
   lcd.setCursor(10, 1);
-  lcd.print(phValue);
+  lcd.print(mapPh);
   lcd.setCursor(13, 1);
   lcd.print("Ph");
+
+  // panggil logikaFuzzy
+  logikaFuzzy();
 
   delay(100);
 }
