719d8ac8de0ec02e4d6611339da8a7b2f0741160
Ammar <59948271+aryannuha@users.noreply.github.com>
2024-11-20 21:55:24 +0700

    Update read_sensor.ino

--- mekatronika_projek/read_sensor.ino
+++ mekatronika_projek/read_sensor.ino
@@ -1,10 +1,14 @@
 void baca_turbidity(){
   turbidityValue = analogRead(turbidity);
   mapT = map(turbidityValue, 610, 0, 0, 100);
-  Serial.println(mapT);
+  Serial.print("Turbidity: ");
+  Serial.print(mapT);
 }
 
 void baca_ph(){
   phValue = analogRead(ph);
-  Serial.println(phValue);
+  voltage = phValue * (vref / adc_res);
+  mapPh = ph_scale * voltage + ph_offset;
+  Serial.print(" | Ph: ");
+  Serial.println(mapPh);
 }
